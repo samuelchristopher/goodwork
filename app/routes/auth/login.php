6f766bb44dba2a82e33665bf31ad8f70661d082a
Samuel Christopher <thesamchris@gmail.com>
2015-07-07 18:14:51 +0800

    Fixed sign in for in-active users

--- app/routes/auth/login.php
+++ app/routes/auth/login.php
@@ -21,9 +21,11 @@ $app->post('/login' , function() use ($app) {
 
   if ($v->passes()) {
     $user = $app->user
-            ->where('email', $identifier)
-            ->orWhere('username', $identifier)
             ->where('active', true)
+            ->where(function($query) use ($identifier) {
+              return $query->where('email', $identifier)
+                    ->orWhere('username', $identifier);
+            })
             ->first();
     if ($user && $app->hash->passwordCheck($password, $user->password)) {
       $_SESSION[$app->config->get('auth.session')] = $user->id;
