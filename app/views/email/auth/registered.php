632895d8fa8ff4898203b934d317f1dcd1e71c70
Samuel Christopher <thesamchris@gmail.com>
2015-07-07 14:58:22 +0800

    Integrated templating in: auth/registered(email).php

--- app/views/email/auth/registered.php
+++ app/views/email/auth/registered.php
@@ -1,1 +1,9 @@
-You have registered.
+{% extends 'email/templates/default.php' %}
+
+{% block lead %}
+  Welcome to the Family!
+{% endblock %}
+
+{% block content %}
+  You have registered!
+{% endblock %}
