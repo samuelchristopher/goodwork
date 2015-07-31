956818f8204bf132c93ca7a80a601b586aac71fa
Samuel Christopher <thesamchris@gmail.com>
2015-07-31 18:17:23 +0800

    Added login modal for home page

--- app/views/home.php
+++ app/views/home.php
@@ -10,7 +10,54 @@
       <div style="padding-left: 20px;">
         <h1>GoodWork Home!</h1>
         <p>Helping the nation, one person at a time. Your next!</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+
+        {% if auth.email %}
+          <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+        {% else %}
+          <p>
+
+            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+              Login
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Login</h4>
+                  </div>
+                  <div class="modal-body">
+                    <form action="{{ urlFor('login.post') }}" method="post">
+                      <div class="form-group">
+                        <label for="identifier">Username/email</label>
+                        <input type="text" class="form-control" id="identifier" name="identifier" placeholder="Username/email">
+                        {% if errors.has('Identifier') %} <div class="alert alert-danger" role="alert"><strong>Oops! </strong>{{ errors.first('Identifier') }}</div> {% endif %}
+                      </div>
+                      <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                        {% if errors.has('Password') %} <div class="alert alert-danger" role="alert"><strong>Oops! </strong>{{ errors.first('Password') }}</div> {% endif %}
+                      </div>
+                      <div class="checkbox">
+                        <label><input type="checkbox" name="remember" id="remember"> Remember me</label>
+                      </div>
+                      <button type="submit" class="btn btn-default">Login</button>
+
+                      <a href="{{ urlFor('password.recover') }}">Forgot your password?</a>
+                      <input type="hidden" name="{{ csrf_key }}" value="{{ csrf_token }}">
+                    </form>
+                  </div>
+                  <!-- <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div> -->
+                </div>
+              </div>
+            </div>
+          </p>
+        {% endif %}
       </div>
     </div>
     <div class="row">
