57cea68afe21b0836084e01caf01bf2c6347eb81
Samuel Christopher <thesamchris@gmail.com>
2015-08-07 12:20:05 +0800

    Fixed mistake

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
-<<<<<<< HEAD
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
-=======
 
 require 'app/start.php';
 
 $app->run();
->>>>>>> 07e0d4510a7426c96f9fa5eef5362e744d22b731
